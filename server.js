57527919718e6d20f039c150014960cbf2deb153
daletalley <dtalley32@icloud.com>
2023-11-14 04:32:21 -0600

    Create API Routes

--- server.js
+++ server.js
@@ -18,4 +18,13 @@ app.get('/notes', (req, res) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
-  
+  const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+app.get('/api/notes', (req, res) => {
+  // Read db.json and return all notes
+});
+
+app.post('/api/notes', (req, res) => {
+  // Add a new note to db.json
+});
