30a38664e045d462532180235d2781ea2f873a86
daletalley <dtalley32@icloud.com>
2023-11-17 16:18:16 -0600

    update express var

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 // apiRoutes.js
-const router = require('express').Router;
+const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 const dbPath = path.join(__dirname, '../db/db.json');
